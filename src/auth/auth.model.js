438df0a8685bea2778c1e73e71550c148ed0f349
VekaTobias10 <vekatobias@outlook.es>
2021-09-06 18:32:48 +0200

    cambiando funciones del auth model

--- src/auth/auth.model.js
+++ src/auth/auth.model.js
@@ -15,11 +15,20 @@ const DATABASE_URL = 'mongodb+srv://immo_company:Q2DfwuJLeS3cyZld@immosensingapp
 /**
  * Obtener el email por token
  */
-export const retrieveEmailByToken = (token) => {
-    return EMAIL_VERIFICATION.find(e => e.token === token)?.email; // si no existe devolvemos undefined
+// export const retrieveEmailByToken = (token) => {
+//     return EMAIL_VERIFICATION.find(e => e.token === token)?.email; // si no existe devolvemos undefined
+// }
+
+
+export async function retrieveEmailByToken(token) {
+    const client = await MongoClient.connect(DATABASE_URL);
+    const token_verification = { token }
+    const data = await client.db('immosensingddbb')
+        .collection('EMAIL_VERIFICATION')
+        .findOne(token_verification)
+    client.close();
+    return data?.email;
 }
-
-
 
 /**
  * registrar token asociado al email
@@ -30,7 +39,7 @@ export async function registerToken(token, email) {
     console.log("token registration" + token + email);
     const client = await MongoClient.connect(DATABASE_URL);
 
-    const verification = { token: token, email: email }
+    const verification = { token, email }
 
     const data = await client.db('immosensingddbb')
         .collection('EMAIL_VERIFICATION')
@@ -51,21 +60,38 @@ export async function registerToken(token, email) {
 
 /**
  * Borramos el token de nuestro modelo
- */
-export const deleteToken = (token) => {
-    const i = EMAIL_VERIFICATION.findIndex(e => e.token === token);
-    if (i >= 0) EMAIL_VERIFICATION.splice(i, 1); // solo borramos si existe
-}
+//  */
+// export const deleteToken = (token) => {
+//     const i = EMAIL_VERIFICATION.findIndex(e => e.token === token);
+//     if (i >= 0) EMAIL_VERIFICATION.splice(i, 1); // solo borramos si existe
+// }
 
+
+export async function  deleteToken (token) {
+
+    const client = await MongoClient.connect(DATABASE_URL);
+
+    const token_verification = {token}
+
+    const data = await client.db('immosensingddbb')
+        .collection('EMAIL_VERIFICATION')
+        .deleteOne(token_verification)
+    client.close();
+    return data;
+}
 /**
  * intentamos obtener el token si es valido devolvemos el email y borramos el token
  */
-export const validateToken = (token) => {
-    const email = retrieveEmailByToken(token);
-    // const email = await retrieveEmailByToken(token);
-    if (email) deleteToken(token);
-    console.log('email', email);
+// export const validateToken = (token) => {
+//     const email = retrieveEmailByToken(token);
+//     if (email) deleteToken(token);
+//     console.log('email', email);
+//     return email;
+// }
+
+
+export async function validateToken(token) {
+    const email = await retrieveEmailByToken(token); //email
+    if (email !== null) await deleteToken(token);
     return email;
 }
-
-
